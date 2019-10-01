6cdfb3d1dbbc8b430392df19fff64907a61b1300
nikehu <1506214574@qq.com>
2019-10-01 12:55:44 +0800

    add sql ddl

--- README.md
+++ README.md
@@ -9,10 +9,27 @@
 [Spring Web文档](https://spring.io/guides/gs/serving-web-content/)
 
 [github OAuth Document](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
+
+[Spring Database](https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/#boot-features-embedded-database-support)
 ## 工具
 [git官网](https://git-scm.com/)
 
 [bootstrap下载](https://v3.bootcss.com/getting-started/#download)
 
 [Visual-paradium](https://www.visual-paradigm.com)
+
+## 脚本
+```sql
+create table USER
+(
+	ID INTEGER primary key AUTO_INCREMENT NOT NULL,
+	ACCOUNT_ID VARCHAR(100),
+	NAME VARCHAR(50),
+	TOKEN CHAR(36),
+	GMT_CREATE BIGINT,
+	GMT_MODIFIED BIGINT
+);
+```
+
+
 
--- src/main/java/com/nicode/community/community/controller/AuthorizeController.java
+++ src/main/java/com/nicode/community/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.nicode.community.community.controller;
 
 import com.nicode.community.community.dto.AccessTokenDTO;
 import com.nicode.community.community.dto.GithubUser;
+import com.nicode.community.community.mapper.UserMapper;
+import com.nicode.community.community.model.User;
 import com.nicode.community.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,11 +11,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 @Controller
 public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
+    @Autowired
+    private UserMapper userMapper;
 
     @Value("${github.client_id}")
     private String client_id;
@@ -24,7 +31,8 @@ public class AuthorizeController {
 
     @GetMapping("callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(client_id);
         accessTokenDTO.setClient_secret(client_secret);
@@ -32,8 +40,22 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirect_uri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        //System.out.println(user.getName());
+        if (githubUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登陆成功，写cookies和session
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        }else{
+            //登陆失败，重新登陆
+            return "redirect:/";
+        }
     }
 }
